29ea964bb825adba21b9e3ef68bceb9b4c08b688
shidalin92@gmail.com <shidalin92@gmail.com>
2018-03-06 00:02:42 +0800

    ~

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,38 +3,38 @@
 </style>
 
 <template>
-    <div class="login" @keydown.enter="handleSubmit">
-        <div class="login-con">
-            <Card :bordered="false">
-                <p slot="title">
-                    <Icon type="log-in"></Icon>
-                    欢迎登录
-                </p>
-                <div class="form-con">
-                    <Form ref="loginForm" :model="form" :rules="rules">
-                        <FormItem prop="userName">
-                            <Input v-model="form.userName" placeholder="请输入用户名">
-                                <span slot="prepend">
+<div class="login" @keydown.enter="handleSubmit">
+  <div class="login-con">
+    <Card :bordered="false">
+      <p slot="title">
+        <Icon type="log-in"></Icon>
+        欢迎登录
+      </p>
+      <div class="form-con">
+        <Form ref="loginForm" :model="form" :rules="rules">
+          <FormItem prop="userName">
+            <Input v-model="form.userName" placeholder="请输入用户名">
+            <span slot="prepend">
                                     <Icon :size="16" type="person"></Icon>
                                 </span>
-                            </Input>
-                        </FormItem>
-                        <FormItem prop="password">
-                            <Input type="password" v-model="form.password" placeholder="请输入密码">
-                                <span slot="prepend">
+            </Input>
+          </FormItem>
+          <FormItem prop="password">
+            <Input type="password" v-model="form.password" placeholder="请输入密码">
+            <span slot="prepend">
                                     <Icon :size="14" type="locked"></Icon>
-                                </span>
-                            </Input>
-                        </FormItem>
-                        <FormItem>
-                            <Button @click="handleSubmit" type="primary" long>登录</Button>
-                        </FormItem>
-                    </Form>
-                    <p class="login-tip">输入admin和admin即可</p>
-                </div>
-            </Card>
-        </div>
-    </div>
+                              </span>
+            </Input>
+          </FormItem>
+          <FormItem>
+            <Button @click="handleSubmit" type="primary" long>登录</Button>
+          </FormItem>
+        </Form>
+        <p class="login-tip">输入admin和admin即可</p>
+      </div>
+    </Card>
+  </div>
+</div>
 </template>
 
 <script>
@@ -49,9 +49,19 @@ export default {
       },
       rules: {
         userName: [
-          { required: true, message: "账号不能为空", trigger: "blur" }
+          {
+            required: true,
+            message: "账号不能为空",
+            trigger: "blur"
+          }
         ],
-        password: [{ required: true, message: "密码不能为空", trigger: "blur" }]
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
